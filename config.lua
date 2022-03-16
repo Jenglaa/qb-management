62c46bdde38e537dc0eeec33f8adc84989f98455
Jenglaa <m.asifelias@gmail.com>
2022-03-17 00:01:24 +0800

    Update config.lua
    add burgershot location

--- config.lua
+++ config.lua
@@ -6,7 +6,8 @@ Config.Jobs = {
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
     ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
-    ['mechanic'] = vector3(-347.41, -133.48, 39.01)
+    ['mechanic'] = vector3(-347.41, -133.48, 39.01),
+    ['burgershot'] = vector3(-1178.17, -896.37, 14.5),
 }
 
 Config.Gangs = {
